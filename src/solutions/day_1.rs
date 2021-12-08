a05e40035679cdcce4ded2002b4f716096cfb802
Peter Blanco <31708924+Qwexerzs@users.noreply.github.com>
2021-12-08 14:13:45 -0500

    Day 1 Puzzle 1 (optimization) (#5)

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,10 +1,8 @@
 pub fn puzzle_1(input: String) -> i32 {
-  let splits = input.split("\n");
   let mut last_number = std::i32::MIN;
   let mut counter = -1;
 
-  for split in splits {
-    let number_string = String::from(split);
+  for number_string in input.lines() {
     let current_number = number_string.parse::<i32>().unwrap();
     if current_number > last_number {
       counter += 1;
