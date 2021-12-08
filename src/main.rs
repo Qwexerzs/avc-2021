fe2a0f3c38aff1da8285dfe1550e8da7e6a2e20f
Peter Blanco <peter.blanco@extend.com>
2021-12-08 15:53:13 -0500

    Day 1 Puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use solutions::*;
 use std::fs;
 
 fn main() {
-    day1();
+    println!("Day 1 Puzzle 1 Output: {}", day1_puzzle1());
+    println!("Day 1 Puzzle 2 Output: {}", day1_puzzle2());
 }
 
-fn day1() {
-    println!(
-        "{}",
-        day_1::puzzle_1(read_file(String::from("input/day_1.txt")))
-    );
+fn day1_puzzle1() -> i32 {
+    day_1::puzzle_1(read_file(String::from("input/day_1_puzzle_1.txt")))
+}
+
+fn day1_puzzle2() -> i32 {
+    day_1::puzzle_2(read_file(String::from("input/day_1_puzzle_2.txt")))
 }
 
 fn read_file(file_name: String) -> String {
